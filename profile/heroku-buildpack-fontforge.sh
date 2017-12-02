090938a7c1f9b2015fa85c648fa0b65756a95abf
Masamitsu MURASE <masamitsu.murase@gmail.com>
2017-12-02 20:28:01 +0900

    Add /usr/local/lib and /usr/lib to LD_LIBRARY_PATH.

--- profile/heroku-buildpack-fontforge.sh
+++ profile/heroku-buildpack-fontforge.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 export PATH=$PATH:$HOME/.heroku/fontforge/usr/local/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.heroku/fontforge/usr/local/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib:$HOME/.heroku/fontforge/usr/local/lib
